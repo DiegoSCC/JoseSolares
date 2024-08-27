53fe6b9aeccfc7b2dce7b871d48c9723fd340d31
DiegoSC <diego.santino03@gmail.com>
2024-08-27 10:41:03 -0300

    Finalizado seccion de tarjetas de info, hero text y se comienza con implementacion de carrrusel con Js

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 
+
 const primaryHeader = document.querySelector(".primary-header");
 const navToggle = document.querySelector(".mobile-nav-toggle");
 const primaryNav = document.querySelector(".primary-nav");
