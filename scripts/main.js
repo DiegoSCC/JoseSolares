79650d91ddd05779114bcf481b350d43c05d42f5
DiegoSC <diego.santino03@gmail.com>
2024-09-26 20:47:36 -0300

    test bootstrap fixes

--- scripts/main.js
+++ scripts/main.js
@@ -1,49 +1,7 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
+
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
-/* let currentIndex = 0;
-const images = document.querySelectorAll('.carousel img');
-const dots = document.querySelectorAll('.dot');
-const totalSlides = images.length;
-let autoplayInterval = 6000; // 3 seconds
-
-function showSlide(index) {
-  images.forEach((img, i) => {
-    img.classList.remove('active');
-    if (i === index) {
-      img.classList.add('active');
-    }
-  });
-  dots.forEach((dot, i) => {
-    dot.classList.remove('active');
-    if (i === index) {
-      dot.classList.add('active');
-    }
-  });
-}
-
-function nextSlide() {
-  currentIndex = (currentIndex + 1) % totalSlides;
-  showSlide(currentIndex);
-}
-
-function prevSlide() {
-  currentIndex = (currentIndex - 1 + totalSlides) % totalSlides;
-  showSlide(currentIndex);
-}
-
-function goToSlide(index) {
-  currentIndex = index;
-  showSlide(currentIndex);
-}
-
-function startAutoplay() {
-  setInterval(nextSlide, autoplayInterval);
-}
-
-// Initialize the first slide and start autoplay
-showSlide(currentIndex);
-/* startAutoplay(); */
 
 
 const video = document.getElementById('video');
